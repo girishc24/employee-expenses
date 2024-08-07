fb1856c203f17398e985ab9d13dd196963296029
Girish C <girishchandraiah1999@gmail.com>
2024-08-07 23:41:57 +0530

    Add a New Features in Admin

--- employeeapp/admin.py
+++ employeeapp/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from  django.contrib.auth.models  import User
-from .models import Employee, Category, Subcategory
+from .models import *
 
 admin.site.register(Employee)
 admin.site.register(Category)
 admin.site.register(Subcategory)
+admin.site.register(Usersubscription)
+admin.site.register(Subscriptions)
+admin.site.register(Help)
+admin.site.register(PrivacyPolicy)
+admin.site.register(Termscondition)
+admin.site.register(Faq)
+admin.site.register(Expense)
+#admin.site.register(ExpenseDocument)
+admin.site.register(Razorpaykey)
+admin.site.register(ExpenseDocument)
+admin.site.register(DeletedAccount)
